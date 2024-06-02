762fa44a42cf45a2db81154baa6f83497c66b1c8
abdullah01 <arfathossen541@gmail.com>
2024-06-02 11:33:05 +0600

    academic faculties controller functionallity  method created

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -16,6 +16,48 @@ const createAcademicFaculty = catchAsync(async (req, res) => {
   })
 })
 
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB()
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculties are retrieved successfully',
+    data: result,
+  })
+})
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved successfully',
+    data: result,
+  })
+})
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    facultyId,
+    req.body,
+  )
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is updated succesfully',
+    data: result,
+  })
+})
+
 export const AcademicFacultyControllers = {
   createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
 }
