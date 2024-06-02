3bf6a114e18fb498627b6955c5b1959f0235c9ed
abdullah01 <arfathossen541@gmail.com>
2024-06-02 11:31:19 +0600

    academic faculties all services method created

--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -6,6 +6,29 @@ const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
   return result
 }
 
+const getAllAcademicFacultiesFromDB = async () => {
+  const result = await AcademicFaculty.find()
+  return result
+}
+
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id)
+  return result
+}
+
+const updateAcademicFacultyIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  })
+  return result
+}
+
 export const AcademicFacultyServices = {
   createAcademicFacultyIntoDB,
+  getAllAcademicFacultiesFromDB,
+  getSingleAcademicFacultyFromDB,
+  updateAcademicFacultyIntoDB,
 }
