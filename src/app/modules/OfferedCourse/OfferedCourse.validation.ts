5591e1b20ee428247cfa598eddc7aef56dd5a177
abdullah01 <arfathossen541@gmail.com>
2024-06-10 14:14:47 +0600

    Validate Time With Hour Min Format

--- src/app/modules/OfferedCourse/OfferedCourse.validation.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.validation.ts
@@ -53,7 +53,7 @@ const updateOfferedCourseValidationSchema = z.object({
     .refine(
       (body) => {
         // startTime : 10:30  => 1970-01-01T10:30
-        //endTime : 12:30  =>  1970-01-01T12:30
+        // endTime  : 12:30  =>  1970-01-01T12:30
 
         const start = new Date(`1970-01-01T${body.startTime}:00`)
         const end = new Date(`1970-01-01T${body.endTime}:00`)
